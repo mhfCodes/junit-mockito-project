918d2d79234831987e1b019e439ef2a3f5c9efca
mhfCodes <mohammadhossein.farrokhi.78@gmail.com>
2022-06-06 22:25:08 +0430

    initialized git

---
+++ src/main/java/com/hossein/JUnitSample/App.java
@@ -0,0 +1,13 @@
+package com.hossein.JUnitSample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
