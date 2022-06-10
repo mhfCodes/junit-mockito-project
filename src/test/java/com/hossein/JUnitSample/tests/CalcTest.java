51e04a4c09b755978fde3267ba77efb6f5ed8457
mhfCodes <mohammadhossein.farrokhi.78@gmail.com>
2022-06-10 22:42:12 +0430

    added some other new tests

--- src/test/java/com/hossein/JUnitSample/tests/CalcTest.java
+++ src/test/java/com/hossein/JUnitSample/tests/CalcTest.java
@@ -10,12 +10,41 @@ class CalcTest {
 	
 	private Calculator calc = new Calculator();
 
+//	@Test
+//	public void test() {
+//		Assert.assertEquals(8, calc.sum(5, 3));
+//		Assert.assertFalse(15 < calc.sub(0, 0));
+//		Assert.assertNotNull(calc.div(5, 4));
+//		Assert.assertTrue(36 == calc.multi(4, 9));
+//	}
+	
 	@Test
-	public void test() {
-		Assert.assertEquals(8, calc.sum(5, 3));
-		Assert.assertFalse(15 < calc.sub(0, 0));
-		Assert.assertNotNull(calc.div(5, 4));
-		Assert.assertTrue(36 == calc.multi(4, 9));
+	public void testAssertions() {
+		String str1 = "hello";
+		String str2 = "hello";
+		String str3 = null;
+		String str4 = "World";
+		String str5 = "World";
+		
+		int num1 = 7;
+		int num2 = 8;
+		
+		String[] arr1 = {"one", "two", "three"};
+		String[] arr2 = {"one", "two", "three"};
+		
+		Assert.assertEquals(str1, str2);
+		
+		Assert.assertTrue(num1 < num2);
+		
+		Assert.assertFalse(num1 > num1);
+		
+		Assert.assertNotNull(str1);
+		
+		Assert.assertNull(str3);
+		
+		Assert.assertNotSame(str1, str3);
+				
 	}
+	
 
 }
